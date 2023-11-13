6226e563690c139051821a3f75bd70ab406c1a30
George Matos <geodm20@gmail.com>
2023-11-12 21:17:12 -0400

    Initial version of Job searcher with raw data in JSON format.

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export function Header(){
+
+    return (
+    <header className="header">
+        <div className="container">
+        <h1>Developer Jobs</h1>
+        </div>
+    </header>
+    )
+}
